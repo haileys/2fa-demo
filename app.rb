77dcb773237f5e061be7c98221f74f4762f18bb7
charliesome <charlie@charliesomerville.com>
2012-03-23 21:12:28 +1100

    oops

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ class User < ActiveRecord::Base
   end
   
   def generate_new_secret!
-    self.secret = rand((32**16)...(32**17-1)).to_s(32).upcase
+    self.secret = rand((32**15)...(32**16-1)).to_s(32).upcase
     @totp = nil
     save!
   end
